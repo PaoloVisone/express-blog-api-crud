88ac38fc7d1259fa7c2970d9eb259dbaf073b318
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-06 16:55:07 +0100

    [feat] add controller

---
+++ controllers/postsController.js
@@ -0,0 +1,24 @@
+const menu = require('../data/posts');
+
+function index(req, res) {
+    // copiamo la logica dell'index
+}
+
+function show(req, res) {
+    // copiamo la logica della show
+}
+
+function store(req, res) {
+    // copiamo la logica della store
+}
+
+function update(req, res) {
+    // copiamo la logica dell'update
+}
+
+function destroy(req, res) {
+    // copiamo la logica della destroy..
+}
+
+// esportiamo tutto
+module.exports = { index, show, store, update, destroy }
