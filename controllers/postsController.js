bf48fd40ac720ee0945e6a606e0fd67dbecc5c90
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-06 17:13:38 +0100

    [feat] Copy the logic

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,22 +2,27 @@ const menu = require('../data/posts');
 
 function index(req, res) {
     // copiamo la logica dell'index
+    res.send('Lista dei post');
 }
 
 function show(req, res) {
     // copiamo la logica della show
+    res.send('Dettagli del post ' + req.params.id);
 }
 
 function store(req, res) {
     // copiamo la logica della store
+    res.send('Creazione nuovo post');
 }
 
 function update(req, res) {
     // copiamo la logica dell'update
+    res.send('Modifica integrale del post ' + req.params.id);
 }
 
 function destroy(req, res) {
     // copiamo la logica della destroy..
+    res.send('Eliminazione del post ' + req.params.id);
 }
 
 // esportiamo tutto
