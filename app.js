463b510a3ad0d022e70d35f8270cf74bcba1a56c
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-06 17:36:26 +0100

    [feat] import router

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const postRouter = require('./routers/posts');
 
+app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use("/posts", postRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
