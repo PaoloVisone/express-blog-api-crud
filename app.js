c147bb86cb2c2b3e84e1684641a5c72a0db5e541
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-10 17:08:44 +0100

    [feat] import not found middlewares

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+// middlewares errorsHandler
 const errorsHandler = require("./middlewares/errorsHandler");
+
+// middlewares notFound
+const notFound = require("./middlewares/notFound");
 
 // Importo il router
 const postRouter = require('./routers/posts');
@@ -17,8 +22,11 @@ app.use(express.json());
 // Rotta
 app.use("/posts", postRouter)
 
-// Registo il middlewares
+// Registo il middlewares errorsHandler
 app.use(errorsHandler);
+
+// Registo il middlewares notFound
+app.use(notFound);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
