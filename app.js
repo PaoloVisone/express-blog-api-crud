921e52dcc8566d605d89cd8d1d2069f1785bdbad
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-10 17:04:28 +0100

    [feat] import errorHandler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const errorsHandler = require("./middlewares/errorsHandler");
 
 // Importo il router
 const postRouter = require('./routers/posts');
@@ -15,6 +16,9 @@ app.use(express.json());
 
 // Rotta
 app.use("/posts", postRouter)
+
+// Registo il middlewares
+app.use(errorsHandler);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
