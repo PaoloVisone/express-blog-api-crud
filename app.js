8a6b78d9af6bb131572159d857d915048570c5ae
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-23 23:14:31 +0100

    [feat] import cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
+const express = require('express')
+const app = express()
 // importiamo il middleware di CORS
-var cors = require('cors');
+const cors = require('cors')
+const port = 3000
+
+// Importo il router
+const postRouter = require('./routers/posts');
 
 // middlewares errorsHandler
 const errorsHandler = require("./middlewares/errorHandler");
@@ -11,19 +13,19 @@ const errorsHandler = require("./middlewares/errorHandler");
 // middlewares notFound
 const notFound = require("./middlewares/notFound");
 
-// Importo il router
-const postRouter = require('./routers/posts');
-
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// registro il middleware di CORS
+app.use(cors({ origin: 'http://localhost:5174' }))
 
 // Registro il body-parser
 app.use(express.json());
 
-// registro il middleware di CORS
-app.use(cors({ origin: 'http://localhost:5174' }))
+// definiamo l'uso di una cartella per i file statici
+app.use(express.static('imgs'));
+
+// definiamo la rotta home
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
 
 // Rotta
 app.use("/posts", postRouter)
